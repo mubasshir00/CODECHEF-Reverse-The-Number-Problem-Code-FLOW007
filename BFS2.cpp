ff958a8debdd5857649b949f6e4e4822aada78b0
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2019-03-29 21:00:00 +0600

    Update BFS2.cpp

--- BFS2.cpp
+++ BFS2.cpp
@@ -1,68 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std ;
-#define N 100000
-vector<int>v[N];
-int visited[1000];
-int level[1000];
-bool BFS(){
-queue<int>q;
-    int src,x,y ;
-    q.push(0);
-    visited[0]=1;
-    level[1]=0;
-    bool isBI =true;
+vector<int>graph[100];
+int visited[100];
+int level[100];
+void bfs(int start)
+{
+    memset(visited,0,sizeof(visited));
+    memset(level,0,sizeof(level));
+    queue<int>q;
+    q.push(start);
+    visited[start]=1;
     while(!q.empty())
     {
-        x = q.front(); q.pop();
-        cout<<x<<endl;
-        for(int i=0;i<v[x].size();i++)
+        int current = q.front();
+        cout<<current<<endl;
+        q.pop();
+        for(int i=0;i<graph[current].size();i++)
         {
-            y = v[x][i];
-            if(visited[y]==0)
+            int next = graph[current][i];
+            if(!visited[next])
             {
-                if(visited[x]==1)
-                {
-                    visited[y]=2;
-                }
-                else
-                {
-                    visited[y]=1;
-                }
-                q.push(y);
-               // visited[y]=1;
-               // level[y] = level[x] +1;
-            }
-            else if(visited[x]==visited[y])
-            {
-            return false;
+                visited[next] =  true ;
+                q.push(next);
+                level[next]=level[current]+1;
             }
 
         }
     }
-    return true ;
+
 }
 
 int main()
 {
-
-    int n,m,x,y ;
-    while(cin>>n)
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
     {
-        if(n==0)break;
-        cin>>m;
-       for(int i=0;i<n;i++)
-       {
-           v[i].clear();
-       }
-    for(int i=1;i<=m;i++)
-    {
+        int x,y ;
         cin>>x>>y;
-        v[x].push_back(y);
-        v[y].push_back(x);
+        graph[x].push_back(y);
+        graph[y].push_back(x);
     }
-    //int visited[N],level[N];
-   BFS();
+    bfs(2);
 
-    }
     return 0 ;
 }
